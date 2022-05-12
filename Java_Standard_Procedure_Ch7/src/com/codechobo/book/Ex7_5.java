1a43a9cd167f000e9b531b23f6d46e3f1b30b23a
ineee199105 <99540516+ineee199105@users.noreply.github.com>
2022-05-12 22:50:48 +0900

    OOP

---
+++ Java_Standard_Procedure_Ch7/src/com/codechobo/book/Ex7_5.java
@@ -0,0 +1,10 @@
+package com.codechobo.book;
+
+public class Ex7_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
